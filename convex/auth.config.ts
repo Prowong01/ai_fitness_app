63f5e16c1c1e2cd5db46f1511906275ee0d0a3c7
“v_fcpwong” <v_fcpwong@tencent.com>
2025-04-25 17:44:51 +0800

    setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://model-weevil-68.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/layout.tsx
@@ -0,0 +1,52 @@
+import { type Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "AI Fitness App",
+  description: "A modern fitness AI platform to get jacked for free",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ConvexClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Navbar />
+
+          {/* GRID BACKGROUND */}
+          <div className="fixed inset-0 -z-10">
+            {" "}
+            <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
+            <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
+          </div>
+
+          <main className="pt-24 flex-grow min-h-screen px-4 sm:px-8">
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </html>
+    </ConvexClerkProvider>
+  );
+}
